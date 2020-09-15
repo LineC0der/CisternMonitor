2fd8e809dfad1052f3b129fbd50c4420586fa3f1
Jonas Volk <jonas.volk@continental-corporation.com>
2020-09-15 14:00:09 +0200

    Started commenting @config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -6,18 +6,18 @@ const char* WIFI_SSID = "SSID";
 const char* WIFI_KEY = "PASSWORD";
 
 //Miscellaneous
-const char* HOSTNAME = "HOSTNAME";
+const char* HOSTNAME = "HOSTNAME";		//Hostname used for OTA-Updated
 const int BAUDRATE = 115200;
-const int CISTERN_RADIUS = 100;
-const int MONTAGE_HEIGHT = 200;
-const int MIN_LEVEL = 10;
-const int DIFF_MIN_MAX_CM = 180;
-const String ue = "\365";
-const int DIGITS_VOLUME = 4;
-const int DIGITS_LEVEL = 3;
+const int CISTERN_RADIUS = 100;			//Radius of the Cistern
+const int MONTAGE_HEIGHT = 200;			//Height of the HC-SR-04-Sensor
+const int MIN_LEVEL = 10;				//Level on which to start the progress bar
+const int DIFF_MIN_MAX_CM = 180;		//Height which will be visualized from the progress bar
+const String ue = "\365";				//German "Umlaut" 'ü'
+const int DIGITS_VOLUME = 4;			//max. Digits needed for the Volume e.g. 6500l
+const int DIGITS_LEVEL = 3;				// -- || -- for level e.g. 175cm
 
 //LCD
-const uint8_t LCD_ADR = 0x3F;
+const uint8_t LCD_ADR = 0x3F;			//I2C-Adress of the LCD (may be differnt)
 const uint8_t LCD_COLS = 20;
 const uint8_t LCD_ROWS = 4;
 const char BAR1[8] = {0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10};  // [|    ]
@@ -28,8 +28,8 @@ const char BAR5[8] = {0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F};  // [||||
 const int PB_WIDTH_BLOCKS = 18;                                         // |[..................]| 
 
 //Timers
-const int TIMER_BACKLIGHT_MS = 60000;
-const int TIMER_USONIC_MS = 10000;
+const int TIMER_BACKLIGHT_MS = 60000;	//backlight duration when turned on
+const int TIMER_USONIC_MS = 10000;		//do a measurments every 10s
 
 //OTA
 const char* OTA_PASSWORD = "PASSWORT";
@@ -38,14 +38,14 @@ const char* OTA_PASSWORD = "PASSWORT";
 const char* MQTT_BROKER = "BROCKER_IP";
 const int MQTT_PORT = 1883;
 const char* MQTT_USER = "USERNAME";
-const char* MQTT_PASSWORD = "MQTT_PASSWORD";
+const char* MQTT_PASSWORD = "MQTT_PASSWORD";	
 const char* TOPIC_LEVEL = "TOPIC_LEVEL";
 const char* TOPIC_VOLUME = "TOPIC_VOLUME";
 
 //Sensors
-const int USONIC_PULSE = 11;
-const int USONIC_PAUSE = 10;
-const int USONIC_CNT = 3;
+const int USONIC_PULSE = 11;		//pulse duration for HC-SR-04 11ms
+const int USONIC_PAUSE = 10;		//wait 10ms before next pulse
+const int USONIC_CNT = 3;			//max. 3 puleses in a row
 
 //Pins
 const uint8_t PIN_MOTION_ECHO = D5;
